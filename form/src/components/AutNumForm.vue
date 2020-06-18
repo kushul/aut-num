34c34402bf3c83c32ea2ad3f4cad646522b6ae74
Kushul Soomaree <kushul@lsl.digital>
2020-06-19 01:02:10 +0400

    built form ui

--- form/src/components/AutNumForm.vue
+++ form/src/components/AutNumForm.vue
@@ -4,24 +4,24 @@
       <v-card-title class="headline cyan lighten-3" dark primary-title>Autonomous System Form</v-card-title>
       <v-card-text class="pa-4">
         <v-form ref="form" v-model="valid" lazy-validation>
-          <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Name" required></v-text-field>
+          <v-text-field prefix="AS" :counter="10" label="aut-num" required></v-text-field>
 
-          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+          <v-text-field label="Notify" required></v-text-field>
 
-          <v-select
-            v-model="select"
-            :items="items"
-            :rules="[v => !!v || 'Item is required']"
-            label="Item"
-            required
-          ></v-select>
+          <v-text-field label="Member of"></v-text-field>
 
-          <v-checkbox
-            v-model="checkbox"
-            :rules="[v => !!v || 'You must agree to continue!']"
-            label="Do you agree?"
-            required
-          ></v-checkbox>
+          <v-text-field label="Remarks"></v-text-field>
+
+          <v-text-field label="ORG"></v-text-field>
+
+          <v-row>
+            <v-col cols="12" sm="6">
+              <v-text-field label="admin-c"></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <v-text-field label="tech-c" required></v-text-field>
+            </v-col>
+          </v-row>
 
           <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">Validate</v-btn>
 
@@ -37,20 +37,7 @@
 <script>
 export default {
   data: () => ({
-    valid: true,
-    name: "",
-    nameRules: [
-      v => !!v || "Name is required",
-      v => (v && v.length <= 10) || "Name must be less than 10 characters"
-    ],
-    email: "",
-    emailRules: [
-      v => !!v || "E-mail is required",
-      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
-    ],
-    select: null,
-    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-    checkbox: false
+    valid: true
   }),
 
   methods: {
