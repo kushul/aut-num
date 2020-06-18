44e9f99d69342ceff6bd2a2cd65c26096bfdcffc
Kushul Soomaree <kushul@lsl.digital>
2020-06-19 00:47:41 +0400

    Header component ui done

---
+++ form/src/components/shared/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar app color="indigo" dark>
+    <v-toolbar-title>Aut-num</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-switch v-model="$vuetify.theme.dark" hide-details inset label="Theme Dark"></v-switch>
+  </v-app-bar>
+</template>
